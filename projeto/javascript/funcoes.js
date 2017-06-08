56c090d8f5bb18bacb25026204a464523fc46b42
raphahenrique <rhf4pf@mail.umkc.edu>
2017-06-08 09:31:19 -0300

    funcionando localmente

--- projeto/javascript/funcoes.js
+++ projeto/javascript/funcoes.js
@@ -13,38 +13,45 @@
   }
 
 
-
-  // gerador de um ID unico e randomico para cada sessao
-  function getUniqueId () {
-    return 'private-' + Math.random().toString(36).substr(2, 9);
-  }
-
-  // funcao para pegar parametro da url (ex: id=abc123 )
-  function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  }
-
-
-
-  //Real time capabilities
-  return new Promise(function (resolve, reject) {
+  	//Real time capabilities
+    return new Promise(function (resolve, reject) {
 
   	//Pusher usando chave gerada ao criar app no Pusher.com 
-    var pusher = new Pusher("2222f56bba340fce1767");
+    var pusher = new Pusher('2222f56bba340fce1767', {
+  						cluster: 'us2'
+										});
 
     // Criando canais to subscribe a modificações
     var channel = pusher.subscribe(id);
 
     channel.bind('client-text-edit', function(html) {
+     
       // save the current position
       var currentCursorPosition = getCaretCharacterOffsetWithin(doc);
       doc.innerHTML = html;
       // set the previous cursor position
       setCaretPosition(doc, currentCursorPosition);
+      
+
     });
+
+    var currentState = pusher.connection.state;
+    $('p#status').text("Pusher status: " + currentState);
+	
+    pusher.connection.bind('state_change', function(states) {
+  		$('p#status').text("Pusher status: " + states.current);
+		
+  	});
+
+
+	channel.bind('pusher:subscription_succeeded', function() {
+  		$('p#subscribe').text("subscription_succeeded");
+	});
+
+	channel.bind('pusher:subscription_error', function() {
+	  	$('p#subscribe').text("subscription_error");
+	});
+
 
     channel.bind('pusher:subscription_succeeded', function() {
       resolve(channel);
@@ -58,6 +65,24 @@
   })
 
 
+
+
+
+
+
+
+  // gerador de um ID unico e randomico para cada sessao
+  function getUniqueId () {
+    return 'private-' + Math.random().toString(36).substr(2, 9);
+  }
+
+  // funcao para pegar parametro da url (ex: id=abc123 )
+  function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
 
 
   //Eventos para a posição do mouse se manter a mesma
---
+++ projeto/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// to serve our JavaScript, CSS and index.html
+app.use(express.static('./'));
+
+var pusher = new Pusher({
+  appId: '350035',
+  key: '2222f56bba340fce1767',
+  secret:  '9ed67b4ea96418de4e04' 
+});
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+
+	//, () => console.log('Listening at http://localhost:5000'));
